7f4db54b759d184bb837ebd75da33819a8e3ef21
Pedro Lisboa <pedrogabriel3@gmail.com>
2022-04-20 01:51:11 -0300

    melhorias

--- app/src/main/java/com/example/agendaapp/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agendaapp/ui/activity/ListaAlunosActivity.java
@@ -14,16 +14,16 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "Jose"));
-        ListView listadealunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listadealunos = findViewById(R.id.activity_lista_alunos_listview);
         listadealunos.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 alunos));
